59aca06095cc076109b8fd2d33f14251372e5590
Jerecho Estolero <jerechoestolero@gmail.com>
2016-11-17 07:12:42 +0800

    Create AlgProg3.10.c

---
+++ AlgProg3.10.c
@@ -0,0 +1,34 @@
+
+/*
+ * Program 3.10 List reversal
+ *
+ * This function reverses the links in a list, returning a pointer to the final
+ * node, which then points to the next-to-final node, and so forth, with the
+ * link in the first node of the original list set to NULL. To accomplish this
+ * task, we need to maintain links to three consecutive nodes in the list.
+ */
+ 
+ typedef struct node* link;
+ struct node {
+  int item;
+  link next;
+};
+
+/*
+ * Function to reverse the links.
+ */
+link reverse(link x)
+{
+  link t;
+  link y=x;
+  link r = NULL;
+  
+  while(y!=NULL)
+  {
+    t=y->next;
+    y->next=r;
+    r=y;
+    y=t;
+  }
+  return r;
+}
